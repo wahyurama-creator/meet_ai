71175a0f195a34faf10edc52e296ae4bb050b781
wahyu rama <wahyuramadhon32@gmail.com>
2025-06-27 20:42:14 +0700

    feat: add responsive dialog component and integrate with dashboard command

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,18 +1,12 @@
 import { db } from "@/db";
 import { agents } from "@/db/schema";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
-// import { TRPCError } from "@trpc/server";
 
 export const agentsRouter = createTRPCRouter({
     getMany: baseProcedure.query(async () => {
         const data = await db
             .select()
             .from(agents);
-
-        // await new Promise(r => setTimeout(r, 3000));
-        // throw new TRPCError({
-        //     code: "BAD_REQUEST",
-        // })
 
         return data;
     })
--- src/modules/dashboard/ui/views/dashboard-command.tsx
+++ src/modules/dashboard/ui/views/dashboard-command.tsx
@@ -1,4 +1,4 @@
-import { CommandDialog, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
+import { CommandInput, CommandItem, CommandList, CommandResponsiveDialog } from "@/components/ui/command";
 import { Dispatch, SetStateAction } from "react";
 
 interface DashboardCommandProps {
@@ -8,7 +8,7 @@ interface DashboardCommandProps {
 
 export const DashboardCommand = ({ open, setOpen }: DashboardCommandProps) => {
     return (
-        <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandResponsiveDialog open={open} onOpenChange={setOpen}>
             <CommandInput
                 placeholder="Find a meeting or agent"
             />
@@ -17,6 +17,6 @@ export const DashboardCommand = ({ open, setOpen }: DashboardCommandProps) => {
                     Item 1
                 </CommandItem>
             </CommandList>
-        </CommandDialog>
+        </CommandResponsiveDialog>
     );
 };
